a69b6737c8cdcee8088c989e58b7cf9ef6eda4c5
Submarine <submarine.court@gmail.com>
2022-09-09 02:20:35 +0300

    Добавили графики

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import streamlit as st
+import pandas
+
+chart = {
+    "Serias1": [1,2,3,4,5],
+    "Serias2": [6,3,4,7,6]
+}
+
+df = pandas.DataFrame(chart)
 
 st.title("Первое приложение на streamlit")
 st.subheader("Публичный сайт на Python")
 st.write("Здесь указан наш текст")
+st.write(df)
+st.area_chart(df)
+st.line_chart(df)
